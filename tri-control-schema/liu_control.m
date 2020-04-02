5c67397b61d1451e4070292c1ae1857e17681226
Bobby <btromm@brandeis.edu>
2020-04-02 00:22:14 -0400

    Add master script

--- tri-control-schema/liu_control.m
+++ tri-control-schema/liu_control.m
@@ -1,39 +1,45 @@
 % Recapitulating Liu et al., 1998
 clear all;
 
-initial_condition_noise = .01;
-
-
 x = xolotl.examples.BurstingNeuron('prefix','liu');
-x.AB.add('LiuSensor');
-
-x.dt = 100;
+x.AB.add('FastLiuSensor');
+x.AB.add('SlowLiuSensor');
+x.AB.add('DCLiuSensor');
 
 % add controllers and their coupling values
 % tau_g = 5e3 ms
-x.AB.ACurrent.add('LiuController', 'A',0,'B',-1,'C',-1);
-x.AB.CaS.add('LiuController', 'A',0,'B',1,'C',0);
-x.AB.CaT.add('LiuController', 'A',0,'B',1,'C',0);
-x.AB.HCurrent.add('LiuController','A',0,'B',1,'C',1);
-x.AB.KCa.add('LiuController','A',0,'B',-1,'C',-1);
-x.AB.Kd.add('LiuController','A',1,'B',-1,'C',0);
-x.AB.Leak.add('LiuController','A',0,'B',0,'C',0);
-x.AB.NaV.add('LiuController','A',1,'B',0,'C',0);
+channels = setdiff(x.AB.find('conductance'),'Leak');
+A = [0 0 0 0 0 1 1];
+B = [-1 1 1 1 -1 -1 0]
+C = [-1 0 0 1 -1 0 0];
 
-x.set('*gbar',initial_condition_noise*rand(8,1));
-x.set('*Controller.m',initial_condition_noise*rand(length(x.get('*Controller.m')*0+1),1));
-channels = x.AB.find('conductance');
-
-data = x.integrate;
 
 %{
-figure('outerposition',[300 300 900 600],'PaperUnits','points','PaperSize',[1200 600]); hold on
-time = x.dt*(1:length(data.AB.V))*1e-3;
-for i = 1:length(channels)
-  if strcmp(channels{i},'Leak')
-    continue
-  end
-  plot(time,data.AB.(channels{i}).gbar
+% optional step to set targets based on xolotl steady-state targets, rather than presets from Liu et al.
 
-figlib.pretty('PlotLineWidth',1.5,'LineWidth',1.5)
+x.t_end = 10e3;
+x.integrate;
+[V,Ca,M] = x.integrate;
+FSD_targets = mean(M);
+F = FSD_targets(1);
+S = FSD_targets(2);
+D = FSD_targets(3);
+
+for i = 1:length(channels)
+  x.AB.(channels{i}.add('LiuController','A',A(i),'B',B(i),'C',C(i),'Fbar',F,'Sbar',S,'Dbar',D));
+end
 %}
+
+
+% use this one for presets (USE ONE OR THE OTHER)
+
+for i = 1:length(channels)
+  x.AB.(channels{i}.add('LiuController','A',A(i),'B',B(i),'C',C(i)));
+end
+
+x.set('*gbar',rand(8,1));
+
+x.t_end = 200e3;
+x.integrate;
+x.t_end = 5e3;
+x.plot;
