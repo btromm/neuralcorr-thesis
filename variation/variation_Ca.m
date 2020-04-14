4a93915d83f8ae5e7c60f22a73aed7df3e26ee7d
bobby <btromm@brandeis.edu>
2020-04-14 14:58:49 -0400

    Add Ca_target noise

---
+++ variation/variation_Ca.m
@@ -0,0 +1,80 @@
+% Figure 3 -- Variation in Ca_target
+
+clear all;
+
+% global parameters
+
+T_measure = 6e3;
+T_grow = 20e3;
+numSim = 500;
+Leak_gbar = 0.2;
+g0 = 1e-1+1e-1*rand(8,1) %set once and fuhget about it
+Ca_target_noise = 0.3;
+
+x = xolotl.examples.neurons.BurstingNeuron('prefix','prinz');
+
+% measure baseline stats
+x.t_end = T_measure;
+x.dt = .1;
+x.integrate;
+x.integrate;
+
+% find calcium target for integral controller
+Ca_target0 = x.AB.Ca_average;
+
+% measure baseline metrics
+x.output_type = 1;
+data = x.integrate;
+metrics0 = xtools.V2metrics(data.AB.V,'sampling_rate',10);
+
+% add controllers
+channels = x.AB.find('conductance');
+leak_cell = {'Leak'};
+for c = 1:length(channels)
+  if ~ismember(channels{c},leak_cell)
+    x.AB.(channels{c}).add('oleary/IntegralController');
+    x.AB.(channels{c}).IntegralController.tau_m = 5e6/x.AB.(channels{c}).gbar;
+  end
+end
+
+x.dt = 0.1;
+x.output_type = 0;
+x.t_end = T_grow;
+x.sim_dt = .1;
+
+gbars = NaN(8,numSim);
+parfor i = 1:numSim
+  disp(i)
+  Ca_target = Ca_target0+(1+randn*Ca_target_noise); %random Ca target (randn allows for + or -)
+  x.set('t_end',T_grow);
+  x.set('*gbar',g0); %same initial conditions every time
+  x.set('*Controller.m',0); %always start m from zero
+  x.set('AB.Leak.gbar',Leak_gbar);
+  x.set('AB.Ca_target',Ca_target)
+  x.integrate;
+
+  % check that it has converged, and that the bursts are OK
+  if abs(x.AB.Ca_target - x.AB.Ca_average)/x.AB.Ca_target > .1
+    disp('Model did not converge')
+    continue
+  end
+
+  % measure metrics
+  x.set('t_end',T_measure);
+  [V,Ca] = x.integrate;
+
+
+  metrics = xtools.V2metrics(V,'sampling_rate',10);
+
+  if (metrics0.burst_period - metrics.burst_period)/metrics0.burst_period > .2
+    disp('Burst periods not OK')
+    continue
+  end
+
+  if (metrics0.duty_cycle_mean - metrics.duty_cycle_mean)/metrics0.duty_cycle_mean > .1
+    disp('Duty cycle not OK')
+    continue
+  end
+
+  gbars(:,i) = x.get('*gbar');
+end
