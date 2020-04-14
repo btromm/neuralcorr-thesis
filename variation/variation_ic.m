3cddb35123a4c31e089db285338a63910e8fe22b
bobby <btromm@brandeis.edu>
2020-04-14 14:53:44 -0400

    Make noise stronger

--- variation/variation_ic.m
+++ variation/variation_ic.m
@@ -8,7 +8,7 @@ T_measure = 6e3;
 T_grow = 20e3;
 numSim = 500;
 Leak_gbar = 0.2
-initial_condition_noise = .01;
+initial_condition_noise = .05;
 
 x = xolotl.examples.neurons.BurstingNeuron('prefix','prinz');
 
