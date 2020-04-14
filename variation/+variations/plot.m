47ffefb02b370c90462c912381ee1edbb2a512c0
bobby <btromm@brandeis.edu>
2020-04-14 14:04:57 -0400

    Fix plot function

--- variation/+variations/plot.m
+++ variation/+variations/plot.m
@@ -1,24 +1,54 @@
 
-function ph = plot(gbars, channels, manipulated)
+function ph = plot(gbars, channels)
 
 % plot all the gs to show correlations
 figure('outerposition',[300 300 1003 1001],'PaperUnits','points','PaperSize',[1003 1001]); hold on
 
+N = length(channels);
+
 c = lines;
 idx = 1;
 
-for i = 1:length(channels)
-	subplot(3,3,i);
-	set(gca,'XScale','linear','YScale','log');
-	hold on
-	for j = 1:length(manipulated)
-		hold on
-		for k = 1:length(gbars(1,:,1))
-			hold on
-			scatter(manipulated(j),gbars(i,k,j),'MarkerFaceColor',c(idx,:),'MarkerEdgeColor',c(idx,:),'MarkerFaceAlpha',.2);
-			hold on
-			%ylabel(channels(i));
+for i = 1:N-1
+	subplot(3,3,i); hold on
+	if(i==7)
+		scatter(gbars(7,:),gbars(8,:));
+		ylabel(channels{8});
+	else
+		scatter(gbars(7,:),gbars(i,:));
+		ylabel(channels{i});
+	end
+		xlabel('Leak');
+end
+sgtitle('Variation in channels vs. variation in Leak')
+
+hold off;
+figure('outerposition',[300 300 1003 1001],'PaperUnits','points','PaperSize',[1003 1001]); hold on
+ax = figlib.gridAxes(N);
+
+c = lines;
+idx = 1;
+
+for i = 1:N-1
+	for j = i+1:N
+		idx = idx + 1;
+		ph(i,j) = scatter(ax(i,j),gbars(i,:),gbars(j,:),'MarkerFaceColor',c(idx,:),'MarkerEdgeColor',c(idx,:),'MarkerFaceAlpha',.2);
+		if j < N
+			set(ax(i,j),'XColor','w');
 		end
+
+		if i > 1
+			set(ax(i,j),'YColor','w');
+		end
+
+		if j == N
+			xlabel(ax(i,j),channels{i});
+		end
+
+		if i == 1
+			ylabel(ax(i,j),channels{j});
+		end
+
 	end
 end
 
