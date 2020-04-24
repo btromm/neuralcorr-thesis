1c7a069f0262a17d33ecd598861b99cf9f3c2599
bobby <btromm@brandeis.edu>
2020-04-24 12:10:33 -0400

    Remove old code

--- variation/+model/gen_channels.m
+++
@@ -1,5 +0,0 @@
-% generate channels variable, that's it
-% used for data analysis
-
-x = xolotl.examples.neurons.BurstingNeuron('prefix','prinz');
-channels = x.AB.find('conductance');
--- variation/+variations/check_successrate.m
+++
@@ -1,7 +0,0 @@
-function [success_rate] = check_successrate(g_proper,numSim)
-
-  ind = find(~isnan(g_proper(1,:)));
-
-  success_rate = (length(ind)/numSim)*100;
-
-  
